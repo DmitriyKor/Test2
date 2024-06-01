545e32d0f0a36af1b5e2ba8f637807a57a372dc8
DmitriyKor <dk@das-ua.com>
2024-06-01 10:10:03 +0300

    Done

---
+++ 3.1.py
@@ -0,0 +1,13 @@
+value1=int(input("Enter 1st figure \n"))
+action=input("Enter action, +, -, * or / \n")
+value2=int(input("Enter 2nd figure \n"))
+if action=='+':
+    result=value1+value2
+elif action=='-':
+    result=value1-value2
+elif action=='*':
+    result=value1*value2
+elif action=='/':
+    if value2!=0:
+      result=value1/value2
+print("Result is ", result)
