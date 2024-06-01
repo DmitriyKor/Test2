e952161e2be16e33e3f28eaa701392c2903ce096
DmitriyKor <dk@das-ua.com>
2024-06-01 10:23:28 +0300

    Done

---
+++ 3.3.py
@@ -0,0 +1,8 @@
+my_list = [12,16,56,6, 66, 78, 89, 67, 89]
+print(my_list)
+half_length=(len(my_list)+1)//2
+my_new_list1=my_list[:half_length]
+print(my_new_list1)
+my_new_list2=my_list[half_length:]
+print(my_new_list2)
+
