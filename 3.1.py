0db82e33e2d48195cf69bbfbf71b7cbc0c46b83d
DmitriyKor <dk@das-ua.com>
2024-06-08 10:47:47 +0300

    Done

--- 3.1.py
+++ 3.1.py
@@ -1,17 +1,20 @@
-value1=int(input("Enter 1st figure \n"))
-action=input("Enter action, +, -, * or / \n")
-value2=int(input("Enter 2nd figure \n"))
-if action=='+':
-    result=value1+value2
-elif action=='-':
-    result=value1-value2
-elif action=='*':
-    result=value1*value2
-elif action=='/':
-    if value2!=0:
-        result=value1/value2
+while True:
+    value1=int(input("Enter 1st figure \n"))
+    action=input("Enter action, +, -, * or / \n")
+    value2=int(input("Enter 2nd figure \n"))
+    if action=='+':
+        result=value1+value2
+    elif action=='-':
+        result=value1-value2
+    elif action=='*':
+        result=value1*value2
+    elif action=='/':
+        if value2!=0:
+            result=value1/value2
+        else:
+            result=None
     else:
         result=None
-else:
-    result=None
-print("Result is ", result)
+    print("Result is ", result)
+    if input("Do you wish to continue? (press Y to continue) \n")!='Y':
+        break
