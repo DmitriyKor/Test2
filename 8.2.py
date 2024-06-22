a5e3d12d32700bdb0e556266ec97a79a5e482a1c
DmitriyKor <dk@das-ua.com>
2024-06-22 10:10:21 +0300

    Done

--- 8.2.py
+++ 8.2.py
@@ -1,9 +1,8 @@
 import string
 
 def is_palindrome(text):
-    left_to_right = ''.join(x.lower() for x in text if not x in string.punctuation+' ')
-    right_to_left = left_to_right[::-1]
-    return left_to_right==right_to_left
+    text_processed = ''.join(x.lower() for x in text if not x in string.punctuation+' ')
+    return text_processed==text_processed[::-1]
 
 assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
 assert is_palindrome('0P') == False, 'Test2'
