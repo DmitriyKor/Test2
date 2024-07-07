457efc7546dc25040f8c9551d3ba03b3b96627f7
DmitriyKor <dk@das-ua.com>
2024-07-07 12:29:34 +0300

    Done

--- 13.1.py
+++ 13.1.py
@@ -6,6 +6,7 @@ class Human:
         self.first_name=first_name
         self.last_name=last_name
 
+
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.gender[0].lower()})"
 
