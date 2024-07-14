b09b32dea56dca4ea3ca0e9a508d159ea2260666
DmitriyKor <dk@das-ua.com>
2024-07-14 18:44:22 +0300

    Done

---
+++ 15.1.py
@@ -0,0 +1,46 @@
+class Rectangle:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_square(self):
+        return self.width * self.height
+
+    def __eq__(self, other):
+        if isinstance(other, Rectangle):
+            return self.get_square() == other.get_square()
+        else:
+            return False
+
+    def __add__(self, other):
+        if isinstance(other, Rectangle):
+            add_width = other.get_square() / self.height
+            return Rectangle(self.width+add_width, self.height)
+        else:
+            return None
+
+    def __mul__(self, n):
+        if isinstance(n, int):
+            return Rectangle(self.width * n, self.height)
+
+    def __str__(self):
+        return f"{self.height}x{self.width}"
+
+
+r1 = Rectangle(2, 4)
+print(r1)
+r2 = Rectangle(3, 6)
+print(r2)
+assert r1.get_square() == 8, 'Test1'
+assert r2.get_square() == 18, 'Test2'
+
+r3 = r1 + r2
+print(r3)
+assert r3.get_square() == 26, 'Test3'
+
+r4 = r1 * 4
+print(r4)
+assert r4.get_square() == 32, 'Test4'
+
+assert Rectangle(3, 6) == Rectangle(2, 9), 'Test5'
